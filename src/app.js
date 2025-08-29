e4276c3cfcef1f0996a25e6916b06b6394951f3b
jerom <jeromghagra@gmail.com>
2025-08-29 19:30:18 +0600

    feat: add helmet middleware for enhanced security

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import helmet from 'helmet';
 import swaggerUi from 'swagger-ui-express';
 
 import routes from './routes/index.js';
@@ -14,6 +15,7 @@ dotenv.config();
 connectDB();
 
 app.use(cors());
+app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
