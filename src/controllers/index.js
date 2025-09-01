14c3532cd067afdddcfd01aa64a9c43f5a8c3691
jerom <jeromghagra@gmail.com>
2025-09-01 22:42:13 +0600

    feat: add order controller with CRUD operations and integrate into index

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,4 +1,5 @@
 export * as authController from './auth.controller.js';
 export * as categoryController from './category.controller.js';
+export * as orderController from './order.controller.js';
 export * as productController from './product.controller.js';
 export * as userController from './user.controller.js';
---
+++ src/controllers/order.controller.js
@@ -0,0 +1,29 @@
+import { orderService } from '../services/index.js';
+import { orderQuery } from '../queries/index.js';
+import { withTransaction } from '../utils/withTransaction.js';
+
+export const createOrder = async (req, res) => {
+  await withTransaction(async (session) => {
+    await orderService.createOrder(req, res, session);
+  });
+};
+
+export const updateOrder = async (req, res) => {
+  await withTransaction(async (session) => {
+    await orderService.updateOrder(req, res, session);
+  });
+};
+
+export const deleteOrder = async (req, res) => {
+  await withTransaction(async (session) => {
+    await orderService.deleteOrder(req, res, session);
+  });
+};
+
+export const getOrderById = async (req, res) => {
+  await orderQuery.getOrderById(req, res);
+};
+
+export const getOrders = async (req, res) => {
+  await orderQuery.getOrders(req, res);
+};
