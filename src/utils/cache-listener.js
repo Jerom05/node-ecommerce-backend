153df22b2ccb9a8e49fb949117bff5d3965341c2
jerom <jeromghagra@gmail.com>
2025-09-04 13:59:08 +0600

    feat: add cache listener for product changes to clear related cache keys

---
+++ src/utils/cache-listener.js
@@ -0,0 +1,11 @@
+import redisClient from './redis-client.js';
+import eventBus from './event-bus.js';
+
+eventBus.on('product:changed', async () => {
+  const keys = await redisClient.keys('*products*');
+  console.log('Event received: product:changed');
+  if (keys.length > 0) {
+    await redisClient.del(keys);
+    console.log(`Cleared ${keys.length} product cache keys (event-driven)`);
+  }
+});
